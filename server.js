e271829c9f4fda03db51554e0df7f490abcae7f2
Claudio Fernando Maciel <claudio.fernando@gmail.com>
2019-07-05 13:11:46 -0300

    from operator.circuit to operator.line

--- server.js
+++ server.js
@@ -1,31 +1,20 @@
 
 const { operator } = require('@railroad/operator');
+
 const SocketLocomotive = require('./locomotives/socket.locomotive');
 const InfluxWagon = require('./wagons/influx.wagon');
 const CourierWagon = require('./wagons/courier.wagon');
 
+
 const station = { pair: process.argv[2], exchange: process.argv[3] }; // LTCBTC
 
-// it comes from process.argv[]
-const circuit = operator.circuit(`${station.exchange}-${station.pair}`);
 
+const line = operator.line(`${station.exchange}-${station.pair}`);
 
-circuit.initialize((machinist, watchdog) => {
-
-  // machinist.guide(); // throws UnimplementedCallbackException 
+line.initialize((machinist, watchdog) => {
 
   machinist.guide({ SocketLocomotive: ['InfluxWagon', 'CourierWagon'] });
-  // machinist.guide({ PubsubLocomotive: ['InfluxWagon', 'CourierWagon'] });
 
   watchdog.watch(SocketLocomotive, InfluxWagon, CourierWagon);
 
 });
-
-// throws AlreadyInitializedException
-// circuit.initialize((machinist, watchdog) => {
-//   machinist.guide({ 'SocketLocomotive': ['InfluxWagon', 'CourierWagon'] });
-//   watchdog.watch(SocketLocomotive, InfluxWagon, CourierWagon);
-// });
-
-
-console.log('available trains: ', JSON.stringify(circuit.trains, null, 1));
