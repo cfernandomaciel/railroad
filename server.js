7d0862612b945eb999794b11f10dede9703e54b2
Claudio Fernando Maciel <claudio.fernando@gmail.com>
2019-07-05 15:35:05 -0300

    from line to station

--- server.js
+++ server.js
@@ -6,12 +6,12 @@ const InfluxWagon = require('./wagons/influx.wagon');
 const CourierWagon = require('./wagons/courier.wagon');
 
 
-const station = { pair: process.argv[2], exchange: process.argv[3] }; // LTCBTC
+const line = { pair: process.argv[2], exchange: process.argv[3] }; // LTCBTC
 
 
-const line = operator.line(`${station.exchange}-${station.pair}`);
+const station = operator.station(`${line.exchange}-${line.pair}`);
 
-line.initialize((machinist, watchdog) => {
+station.initialize((machinist, watchdog) => {
 
   machinist.guide({ SocketLocomotive: ['InfluxWagon', 'CourierWagon'] });
 
