ecbf01fbe0db25a1844181c17f92b71f4914c5d4
Claudio Fernando Maciel <claudio.fernando@gmail.com>
2019-07-05 09:59:21 -0300

    fixed Object.assign for modules problem

--- server.js
+++ server.js
@@ -2,15 +2,9 @@
 const { operator } = require('@railroad/operator');
 const SocketLocomotive = require('./locomotives/socket.locomotive');
 const InfluxWagon = require('./wagons/influx.wagon');
+const CourierWagon = require('./wagons/courier.wagon');
 
 const station = { pair: process.argv[2], exchange: process.argv[3] }; // LTCBTC
-
-const CourierWagon = {
-  name: 'CourierWagon',
-  init: () => {
-    console.log('initializing CourierWagon');
-  },
-};
 
 // it comes from process.argv[]
 const circuit = operator.circuit(`${station.exchange}-${station.pair}`);
@@ -23,10 +17,7 @@ circuit.initialize((machinist, watchdog) => {
   machinist.guide({ SocketLocomotive: ['InfluxWagon', 'CourierWagon'] });
   // machinist.guide({ PubsubLocomotive: ['InfluxWagon', 'CourierWagon'] });
 
-  const socketLocomotive = Object.assign({}, SocketLocomotive());
-  const influxWagon = Object.assign({}, InfluxWagon());
-
-  watchdog.watch(socketLocomotive, influxWagon, CourierWagon);
+  watchdog.watch(SocketLocomotive, InfluxWagon, CourierWagon);
 
 });
 
--- wagons/courier.wagon.js
+++ wagons/courier.wagon.js
@@ -1,50 +1,52 @@
 
 const { CourierConnector, KafkaConsumer } = require('@railroad/wagon');
-const { Station } = require('@railroad/station');
+// const { Station } = require('@railroad/station');
 
-const config = require('./config');
+// const config = require('./config');
 
-const stationFile = require('./station');
+// const stationFile = require('./station');
 
-const topics = Station.load(stationFile).topics;
+// const topics = Station.load(stationFile).topics;
 
-let consumer = null;
-let Courier  = null;
+// let consumer = null;
+// let Courier  = null;
 
-let trade   = null;
-let candle  = null;
-let ticker  = null;
-let depth   = null;
+// let trade   = null;
+// let candle  = null;
+// let ticker  = null;
+// let depth   = null;
 
 
 const CourierWagon = () => ({
+  name: 'CourierWagon',
   init: () => {
 
-    Courier = CourierConnector.new({ host: config.courier.host });
-    consumer = new KafkaConsumer({ host: config.kafka.host });
+    console.log('unimplemented module');
+    // Courier = CourierConnector.new({ host: config.courier.host });
+    // consumer = new KafkaConsumer({ host: config.kafka.host });
 
-    trade   = await consumer.instanceOf(Station.extractSingle(topics, 'trade')).connect();
-    // candle  = await consumer.instanceOf(Station.extractSingle(topics, 'candle')).connect();
-    // ticker  = await consumer.instanceOf(Station.extractSingle(topics, 'ticker')).connect();
-    // depth   = await consumer.instanceOf(Station.extractSingle(topics, 'depth')).connect();
+    // trade   = await consumer.instanceOf(Station.extractSingle(topics, 'trade')).connect();
+    // // candle  = await consumer.instanceOf(Station.extractSingle(topics, 'candle')).connect();
+    // // ticker  = await consumer.instanceOf(Station.extractSingle(topics, 'ticker')).connect();
+    // // depth   = await consumer.instanceOf(Station.extractSingle(topics, 'depth')).connect();
 
-    trade.on('message', (err, msg) => {
+    // trade.on('message', (err, msg) => {
 
-      if(err) throw err;
+    //   if(err) throw err;
 
-      courier = await Courier.send(Station.extractSingle(topics, 'trade'), msg);
+    //   courier = await Courier.send(Station.extractSingle(topics, 'trade'), msg);
 
-      courier.on('success', msg => console.log);
-      courier.on('error', err => console.error);
+    //   courier.on('success', msg => console.log);
+    //   courier.on('error', err => console.error);
 
-    });
+    // });
 
-    trade.on('error', (err) => {
-      //DO Whatever necessary...
-    });
+    // trade.on('error', (err) => {
+    //   //DO Whatever necessary...
+    // });
 
     
-  }
+  },
 });
 
 module.exports = CourierWagon;
