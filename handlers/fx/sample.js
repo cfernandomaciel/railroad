812432b9871068bd012b2bfa69bc279f021cf195
Claudio Fernando Maciel <claudio.fernando@gmail.com>
2019-07-25 20:44:45 -0300

    function faas object model impl

---
+++ handlers/fx/sample.js
@@ -0,0 +1,7 @@
+module.exports.hello = () => {
+  return 'Hello World!';
+};
+
+module.exports.calculate = (a, b) => {
+  return a + b;
+};
--- server.js
+++ server.js
@@ -4,13 +4,26 @@ const { operator } = require('@railroad/operator');
 const SampleLocomotive = require('./locomotives/sample.locomotive');
 const SampleWagon = require('./wagons/sample.wagon');
 
+const config = require('./config');
+const functions = require('./functions.map.json');
+
+const faasConfig = config.faas;
+const { host } = config;
+
+
 const stationName = process.argv[2];
 const station = operator.station(stationName);
 
-station.initialize((machinist, watchdog) => {
+const serverlessPath = 'serverless.yml';
 
+station.initialize((machinist, watchdog, commuter) => {
+  
   machinist.guide({ SampleLocomotive: ['SampleWagon'] });
 
+  commuter.faas({
+    serverlessFileNamePath: serverlessPath, faasConfig, host, functions, 
+  });
+  
   watchdog.watch(SampleLocomotive, SampleWagon);
-
+  
 });
